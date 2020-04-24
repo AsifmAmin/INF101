2837b32773499dbcbd9b160625125aba27c40a5a
Mohammad Asif Amin <Mohammad.Amin@student.uib.no>
2020-04-24 04:45:49 +0200

    javadoc

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import Controller.InputOutputController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        InputOutputController run = new InputOutputController();
+        run.welcomeMessage();
+    }
+}
